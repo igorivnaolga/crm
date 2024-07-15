5fbe28d62e8e83008308f8fecc2cc66012c6b178
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-07-16 02:54:15 +0300

    add modal, form, add company button

---
+++ src/app/components/add-company-button.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React, { useState } from 'react';
+import Button from './button';
+import CompanyFormModal from './company-form-modal';
+
+export default function AddCompanyButton() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setShow(true)}>Add company</Button>
+      <CompanyFormModal
+        onSubmit={console.log}
+        show={show}
+        onClose={() => setShow(false)}
+      />
+    </>
+  );
+}
